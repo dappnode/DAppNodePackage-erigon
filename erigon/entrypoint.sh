2c8c4befdb844ba21864c1f5ec95567eb4c46129
pablo <mendez4a@gmail.com>
2021-08-24 18:07:28 +0200

    fix entrypoint

--- erigon/entrypoint.sh
+++ erigon/entrypoint.sh
@@ -7,23 +7,17 @@
 # DAPPNODE: v0.1.7 to v0.1.8
 # Datadir migration must be done manually according to https://github.com/ledgerwatch/erigon/releases/tag/v2021.08.03
 
-NEW_CHAINDATA_DIR="/home/erigon/.local/share"
-OLD_CHAINDATA_DIR="/var/lib/erigon/erigon"
+DATADIR="/home/erigon/.local/share"
 
-if [ -d "$OLD_CHAINDATA_DIR" ]; then 
-    echo "Migrating chaindatadir from ${OLD_CHAINDATA_DIR} to ${NEW_CHAINDATA_DIR}"
-    [ ! -d "$OLD_CHAINDATA_DIR/chaindata" ] && { echo "Cannot perform migration, old chaindatadir ${OLD_CHAINDATA_DIR}/chaindata must exist"; exit 1 }
-    mkdir -p "$NEW_CHAINDATA_DIR"
-    mv "${OLD_CHAINDATA_DIR}/chaindata" "$NEW_CHAINDATA_DIR"
-    echo "Migration done"
-    rm -rf "$OLD_CHAINDATA_DIR"
-done
+if [ ! -d "$DATADIR/erigon/chaindata" ]; then
+    mv "$DATADIR/erigon/chaindata" "$DATADIR"
+fi
 
 ##########
 # Erigon #
 ##########
 
-exec erigon --datadir=/home/erigon/.local/share \
+exec erigon --datadir=${DATADIR} \
     --metrics \
     --metrics.addr=\"0.0.0.0\" \
     --metrics.port=\"6060\" \
@@ -32,4 +26,3 @@ exec erigon --datadir=/home/erigon/.local/share \
     --pprof.addr=0.0.0.0 \
     --pprof.port=6061 \
     ${ERIGON_EXTRA_OPTS}
-
