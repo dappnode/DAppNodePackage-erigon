d8e60bdf9405aa31b10304744fa77d83b174dd96
eduadiez <eduadiez@gmail.com>
2022-06-29 16:36:20 +0200

    remove rpcdaemon service

--- erigon/entrypoint.sh
+++ erigon/entrypoint.sh
@@ -7,6 +7,8 @@
 # DAPPNODE: v0.1.7 to v0.1.8
 # Datadir migration must be done manually according to https://github.com/ledgerwatch/erigon/releases/tag/v2021.08.03
 
+PORT="${P2P_PORT:=30303}"
+
 DATADIR="/home/erigon/.local/share"
 
 if [ -d "$DATADIR/erigon/chaindata" ]; then
@@ -35,11 +37,16 @@ fi
 ##########
 
 exec erigon --datadir=${DATADIR} \
+    --http.addr=0.0.0.0 \
+    --http.vhosts=* \
+    --http.corsdomain=* \
+    --ws \
+    --private.api.addr=0.0.0.0:9090 \
     --metrics \
     --metrics.addr=0.0.0.0 \
     --metrics.port=6060 \
-    --private.api.addr=0.0.0.0:9090 \
     --pprof \
     --pprof.addr=0.0.0.0 \
     --pprof.port=6061 \
+    --port=${PORT} \
     ${ERIGON_EXTRA_OPTS}
